d5f66001709143a1b3eee83bc5bc176a9be05959
everything411 <everything411@163.com>
2021-12-13 22:00:09 +0800

    .net fx4.8, x64 build, dpiaware

--- GenshinAccount/FormInput.Designer.cs
+++ GenshinAccount/FormInput.Designer.cs
@@ -29,7 +29,6 @@ namespace GenshinAccount
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormInput));
             this.label1 = new System.Windows.Forms.Label();
             this.txtAcctName = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
@@ -39,24 +38,27 @@ namespace GenshinAccount
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 24);
+            this.label1.Location = new System.Drawing.Point(64, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(242, 18);
+            this.label1.Size = new System.Drawing.Size(244, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "请输入当前登录账号的备注：";
             // 
             // txtAcctName
             // 
-            this.txtAcctName.Location = new System.Drawing.Point(12, 56);
+            this.txtAcctName.Location = new System.Drawing.Point(15, 75);
+            this.txtAcctName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtAcctName.Name = "txtAcctName";
-            this.txtAcctName.Size = new System.Drawing.Size(338, 28);
+            this.txtAcctName.Size = new System.Drawing.Size(412, 31);
             this.txtAcctName.TabIndex = 1;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(80, 131);
+            this.btnSave.Location = new System.Drawing.Point(98, 175);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(196, 32);
+            this.btnSave.Size = new System.Drawing.Size(240, 43);
             this.btnSave.TabIndex = 2;
             this.btnSave.Text = "保存";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -65,23 +67,26 @@ namespace GenshinAccount
             // chkSaveSettings
             // 
             this.chkSaveSettings.AutoSize = true;
-            this.chkSaveSettings.Location = new System.Drawing.Point(62, 99);
+            this.chkSaveSettings.Location = new System.Drawing.Point(76, 132);
+            this.chkSaveSettings.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chkSaveSettings.Name = "chkSaveSettings";
-            this.chkSaveSettings.Size = new System.Drawing.Size(214, 22);
+            this.chkSaveSettings.Size = new System.Drawing.Size(216, 28);
             this.chkSaveSettings.TabIndex = 3;
             this.chkSaveSettings.Text = "保存当前原神游戏设置";
             this.chkSaveSettings.UseVisualStyleBackColor = true;
             // 
             // FormInput
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(370, 175);
+            this.ClientSize = new System.Drawing.Size(452, 233);
             this.Controls.Add(this.chkSaveSettings);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.txtAcctName);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Icon = global::GenshinAccount.Properties.Resources.hutao;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormInput";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "新增账号";
